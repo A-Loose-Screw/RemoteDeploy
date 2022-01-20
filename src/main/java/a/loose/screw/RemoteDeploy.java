81c7f46d86cf955e815124fdf26cad97f10850d8
CJBuchel <connorbuchel@gmail.com>
2022-01-20 13:31:31 +0800

    Added logback lib:

--- src/main/java/a/loose/screw/RemoteDeploy.java
+++ src/main/java/a/loose/screw/RemoteDeploy.java
@@ -3,13 +3,18 @@ package a.loose.screw;
 import org.gradle.api.Project;
 import org.gradle.api.Plugin;
 
+// Local
+import a.loose.screw.Logback;
+
 /**
  * Greeting class plugin for gradle.
  * Creates a class called greeting.
  */
 public class RemoteDeploy implements Plugin<Project> {
+  public static Logback logback;
   public void apply(Project project) {
     project.getTasks().register("greeting", task -> {
+      logback.print();
       task.doLast(s -> System.out.println("Hello from gradle plugin a.loose.screw.greeting"));
     });
   }
