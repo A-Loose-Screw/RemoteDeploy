ba3f3fd34cbb2573829ebe3e4fc9b1c6f7fef44b
CJBuchel <connorbuchel@gmail.com>
2022-12-29 13:33:32 +0200

    Added the syntax required for the build script

--- src/main/java/a/loose/screw/deploy/DeployExtension.java
+++ src/main/java/a/loose/screw/deploy/DeployExtension.java
@@ -1,32 +1,34 @@
 package a.loose.screw.deploy;
 
 import org.gradle.api.Action;
+import org.gradle.api.NamedDomainObjectContainer;
 import org.gradle.api.Project;
 
 import a.loose.screw.logging.RDLogger;
 
 public class DeployExtension {
-  private Target _target;
+  // private Target _target;
+  private NamedDomainObjectContainer<Target> _targets;
   private Project _project;
   private RDLogger _logger;
 
   public DeployExtension(Project project, RDLogger logger) {
     this._project = project;
     this._logger = logger;
+    this._targets = project.container(Target.class);
   }
 
-  public Target target(String name, Class<? extends Target> type, final Action<? extends Target> config) {
-    // Target target = _project.getObjects().newInstance(type, name, this._project);
-    Target target = _project.getObjects().newInstance(type);
+  public Target target(String name, final Action<? super Target> config) {
+    Target target = _targets.create(name);
 
+    config.execute(target);
+    this._targets.add(target);
     this._logger.log(name);
-    
-    // config.execute(target);
-    this._target = target;
     return target;
   }
 
-  public Target target(String name, final Action<? extends Target> config) {
-    return target(name, Target.class, config);
+  public NamedDomainObjectContainer<Target> targets(Action<? super NamedDomainObjectContainer<Target>> config) {
+    config.execute(this._targets);
+    return this._targets;
   }
 }
--- src/main/java/a/loose/screw/deploy/Target.java
+++ src/main/java/a/loose/screw/deploy/Target.java
@@ -1,5 +1,25 @@
 package a.loose.screw.deploy;
 
-public class Target {
+import org.gradle.api.NamedDomainObjectContainer;
+
+import javax.inject.Inject;
+
+import org.gradle.api.Named;
+
+public class Target implements Named {
+  private String _name;
+
+  @Inject
+  public Target(String name) {
+    this._name = name;
+  }
+
+  @Override
+  public String getName() {
+    return _name;
+  }
+
+
   public String host;
+
 }
